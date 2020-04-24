619df50314f918bbe4d9d0e797a73e0e509167d4
Gzuwkj <1838679406@qq.com>
2020-04-24 23:40:52 +0800

    111

---
+++ Cluster_1/Master/modoverrides.lua
@@ -0,0 +1,34 @@
+return {
+  ["workshop-1207269058"]={ configuration_options={  }, enabled=true },
+  ["workshop-347079953"]={
+    configuration_options={ DFV_Language="EN", DFV_MinimalMode="default" },
+    enabled=true 
+  },
+  ["workshop-375850593"]={ configuration_options={  }, enabled=true },
+  ["workshop-501385076"]={ configuration_options={ quick_harvest=true }, enabled=true },
+  ["workshop-623749604"]={
+    configuration_options={
+      Craft="Normal",
+      Destroyable="DestroyByAll",
+      FoodSpoilage=1,
+      Language="En",
+      Position="Center",
+      Slots=80 
+    },
+    enabled=true 
+  },
+  ["workshop-666155465"]={
+    configuration_options={
+      chestB=-1,
+      chestG=-1,
+      chestR=-1,
+      food_estimation=-1,
+      food_order=0,
+      food_style=0,
+      lang="auto",
+      show_food_units=-1,
+      show_uses=-1 
+    },
+    enabled=true 
+  } 
+}
